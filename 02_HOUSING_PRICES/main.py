2e7941afad07c15f3c7e0465c07ee24fb3d0da26
lucasamtaylor01 <lucasamtaylor01@proton.me>
2026-07-02 14:35:26 -0300

    refactor: move data cleaning logic to main.py and cleaning.py
    - Apply the notebook's data cleaning logic in standalone .py files
    - Generate test_clean.csv and train_clean.csv as output

---
+++ 02_HOUSING_PRICES/main.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import pandas as pd
+from scripts.cleaning import cleaning
+
+# PATH DEFINITIONS
+
+BASE_DIR = Path().resolve()
+
+DATA_DIR = BASE_DIR / "data"
+RAW_DATA_DIR = DATA_DIR / "data_raw"
+
+CLEAN_DATA_DIR = DATA_DIR / "data_clean"
+CLEAN_DATA_DIR.mkdir(exist_ok=True)
+
+
+
+def main():
+
+    df_train_raw = pd.read_csv(RAW_DATA_DIR / "train.csv")
+    df_test_raw = pd.read_csv(RAW_DATA_DIR / "test.csv")
+
+    print("\nSTARTING DATA CLEANING PROCESS...\n\n")
+    df_train_clean = cleaning(df_train_raw, 0)
+    df_train_clean.to_csv(CLEAN_DATA_DIR / 'train_clean.csv', index=False)
+
+    df_test_clean = cleaning(df_test_raw, 1)
+    df_test_clean.to_csv(CLEAN_DATA_DIR / 'test_clean.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 02_HOUSING_PRICES/scripts/cleaning.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import numpy as np
+
+
+def _string_treatment(df_raw: pd.DataFrame) -> pd.DataFrame:
+    """This function uppercases the DataFrame's column names and string values.
+    It should be used JUST on cleaning().
+
+    Args:
+        df_raw (pd.DataFrame): original DataFrame
+
+    Returns:
+        pd.DataFrame: Dataframe with column names and string values uppercased
+    """
+
+    df_str_treat = df_raw.copy()
+    df_str_treat.columns = df_str_treat.columns.str.upper()
+
+    columns_str = df_str_treat.select_dtypes(include="string")
+
+    for col in columns_str.columns:
+        df_str_treat[col] = df_str_treat[col].str.upper()
+
+    return df_str_treat
+
+
+def _null_treatment(df_str_treat: pd.DataFrame) -> pd.DataFrame:
+    """This function treats null values of DataFrame, filling or dropping them depending
+    on the column, and also drops rows with inconsistent basement data.
+    It should be used JUST on cleaning().
+
+    Args:
+        df_str_treat (pd.DataFrame): Dataframe with string treated
+
+    Returns:
+        pd.DataFrame: Dataframe with null values treated
+    """
+
+    df_null_treat = df_str_treat.copy()
+
+    # GARAGE TREATMENT
+    df_null_treat["HASGARAGE"] = np.where(
+        df_null_treat["GARAGEYRBLT"].isnull(), 0, 1
+    ).astype("bool")
+    df_null_treat = df_null_treat.drop(columns="GARAGEYRBLT")
+
+    df_null_treat["GARAGEQUAL"] = df_null_treat["GARAGEQUAL"].fillna("NA")
+    df_null_treat["GARAGEFINISH"] = df_null_treat["GARAGEFINISH"].fillna("NA")
+    df_null_treat["GARAGETYPE"] = df_null_treat["GARAGETYPE"].fillna("NA")
+    df_null_treat["GARAGECOND"] = df_null_treat["GARAGECOND"].fillna("NA")
+
+    # BASEMENT TREATMENT
+    df_null_treat["BSMTCOND"] = df_null_treat["BSMTCOND"].fillna("NA")
+    df_null_treat["BSMTQUAL"] = df_null_treat["BSMTQUAL"].fillna("NA")
+    df_null_treat["BSMTFINTYPE1"] = df_null_treat["BSMTFINTYPE1"].fillna("NA")
+
+    bsmt_columns = [
+        "BSMTFINTYPE2",
+        "BSMTEXPOSURE",
+        "BSMTCOND",
+        "BSMTQUAL",
+        "BSMTFINTYPE1",
+    ]
+    df_null_treat_bsmt = df_null_treat[bsmt_columns].copy()
+
+    mask = (df_null_treat_bsmt["BSMTCOND"].notnull()) & (
+        (df_null_treat_bsmt["BSMTEXPOSURE"].isnull())
+        | (df_null_treat_bsmt["BSMTFINTYPE2"].isnull())
+    )
+    filter = ~mask
+    df_null_treat = df_null_treat[filter].reset_index(drop=True)
+
+    df_null_treat["BSMTFINTYPE2"] = df_null_treat["BSMTFINTYPE2"].fillna("NA")
+    df_null_treat["BSMTEXPOSURE"] = df_null_treat["BSMTEXPOSURE"].fillna("NA")
+
+    # POOL TREATMENT
+
+    df_null_treat["HASPOOL"] = np.where(df_null_treat["POOLQC"].isnull(), 0, 1).astype(
+        "bool"
+    )
+    df_null_treat = df_null_treat.drop(columns=["POOLQC", "POOLAREA"])
+
+    # FIREPLACE TREATMENT
+    df_null_treat["FIREPLACEQU"] = df_null_treat["FIREPLACEQU"].fillna("NA")
+
+    # OTHERS
+    df_null_treat = df_null_treat.dropna(subset=["ELECTRICAL", "MASVNRAREA"])
+    df_null_treat["MISCFEATURE"] = df_null_treat["MISCFEATURE"].fillna("NA")
+    df_null_treat["ALLEY"] = df_null_treat["ALLEY"].fillna("NA")
+    df_null_treat["FENCE"] = df_null_treat["FENCE"].fillna("NA")
+    df_null_treat["MASVNRTYPE"] = df_null_treat["MASVNRTYPE"].fillna("None")
+    df_null_treat["LOTFRONTAGE"] = df_null_treat["LOTFRONTAGE"].fillna(0)
+
+    return df_null_treat
+
+
+def _types_treatment(df_null_treat: pd.DataFrame) -> pd.DataFrame:
+    """Corrects types of DataFrame. It should be used JUST on cleaning().
+
+    Args:
+        df_null_treat (pd.DataFrame): Dataframe with null values treated
+
+    Returns:
+        pd.DataFrame: Dataframe with types treated
+    """
+    df_type_treat = df_null_treat.copy()
+    measure_varibles = [
+        "LOTAREA",
+        "BSMTFINSF1",
+        "BSMTFINSF2",
+        "BSMTUNFSF",
+        "TOTALBSMTSF",
+        "1STFLRSF",
+        "2NDFLRSF",
+        "LOWQUALFINSF",
+        "GRLIVAREA",
+        "GARAGEAREA",
+        "WOODDECKSF",
+        "OPENPORCHSF",
+        "ENCLOSEDPORCH",
+        "3SSNPORCH",
+        "SCREENPORCH",
+    ]
+
+    df_type_treat[measure_varibles] = df_type_treat[measure_varibles].astype("float64")
+
+    return df_type_treat
+
+
+def cleaning(df_raw: pd.DataFrame, df_type: int) -> pd.DataFrame:
+    try:
+        if df_type == 0:
+            print("[!] STARTING DATA CLEANING PROCESS OF TRAIN DATAFRAME ...\n")
+        else:
+            print("[!] STARTING DATA CLEANING PROCESS OF TEST DATAFRAME ...\n")
+
+        df_str_treat = _string_treatment(df_raw)
+        print("[1/3] STRINGS TREATED")
+
+        df_null_treat = _null_treatment(df_str_treat)
+        print("[2/3] NULL VALUES TREATED")
+
+        df_clean = _types_treatment(df_null_treat)
+        print("[3/3] TYPES TREATED\n")
+
+        if df_type == 0:
+            print("[!] DATA CLEANING PROCESS OF TRAIN DATAFRAME  WAS SUCCESSFUL...\n")
+        else:
+            print("[!] DATA CLEANING PROCESS OF TEST DATAFRAME  WAS SUCCESSFUL...\n")
+
+        print(75*'-', '\n')
+        return df_clean
+    
+    except:
+        print("[!] AN ERROR WAS OCURRED ON DATA CLEANING PROCESS. \n")
